43b5576df3b9187a353248b86da0f5eef1253971
christophertdoc <christophertdoc@gmail.com>
2020-08-05 14:55:25 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react' 
 import { AuthContext } from './contexts/AuthContext'
+import './App.css';
 
 const App = () => {
 	const { authenticated, toggleAuth } = useContext(AuthContext)
 	return (
-		<div className='App'>	
-			<div onClick={ toggleAuth }>{ authenticated ? 'Logged In' : 'Logged Out' }	</div>	
-		</div>	
+		<div className="App">	
+			<div className="button" onClick={ toggleAuth }>{ authenticated ? 'Logged In' : 'Logged Out' }</div>	
+		</div>
 	)	
 }
 
